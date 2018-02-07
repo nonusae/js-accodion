4134386b694708518ac4534e2d5b7292134b837f
Natthakit <nonusae@gmail.com>
2018-02-07 11:22:57 +0700

    First implementation

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const accordionContainer = document.querySelector('.jsAccordionContainer')
+accordionContainer.addEventListener('click', e =>
+  {
+    const header = e.target.closest('.jsAccordionHeader')
+    if (header) {
+      header.parentNode.classList.toggle('is-open')
+    }
+  }
+)
