1e78064b7181c35eb3fc37ce7bab485116e28f3b
Natthakit <nonusae@gmail.com>
2018-03-12 12:45:36 +0700

    Add animation to accordion

--- js/main.js
+++ js/main.js
@@ -2,8 +2,19 @@ const accordionContainer = document.querySelector('.jsAccordionContainer')
 accordionContainer.addEventListener('click', e =>
   {
     const header = e.target.closest('.jsAccordionHeader')
-    if (header) {
-      header.parentNode.classList.toggle('is-open')
+    if (!header) return
+
+    const accordion = header.parentNode
+    const accordionContent = header.nextElementSibling
+    const accordionInner = accordionContent.firstElementChild
+    const height = accordionInner.getBoundingClientRect().height
+
+    if  (accordion.classList.contains('is-open')) {
+      accordionContent.style.height = '0px'
+      accordion.classList.remove('is-open')
+    } else {
+      accordionContent.style.height = height + 'px';
+      accordion.classList.add('is-open');
     }
   }
 )
